29cce15f4a8b506df53aa1be5fca81e355f6d231
Zachary Teper <zachary.teper@gmail.com>
2021-03-20 15:12:57 -0400

    Update the autovid.sh script
    When the Pi boots up, a new video with the current timestamp
    in its file name will be created. Thus, restarting the Pi
    multiple times will not cause the previous video file to
    be overwritten.

--- inflight_recording/autovid.sh
+++ inflight_recording/autovid.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-VIDEO_FILE="/home/pi/autovid/video.h264"
+NOW=$(date +%Y%m%d%H%M%S)
 
-if [ -f "$VIDEO_FILE" ]; then
-    rm $VIDEO_FILE
-fi
+VIDEO_FILE="/home/pi/autovid/video$NOW.h264"
 
-# Set timeout to zero to enable continous recording.
-# Flush output buffer to ensure that the entire 
-# recording is saved to disk.
 raspivid -t 0 -fl -o $VIDEO_FILE
+
+#end
