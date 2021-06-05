3d5485dff8287d6d7706200b7a6c3ec69faed593
Chris Sankey <5505109+chrissank@users.noreply.github.com>
2021-06-04 23:10:08 -0400

    Add dynamic video title

--- inflight_recording/autovid.sh
+++ inflight_recording/autovid.sh
@@ -4,6 +4,6 @@ NOW=$(date +%Y%m%d%H%M%S)
 
 VIDEO_FILE="/home/pi/autovid/video$NOW.h264"
 
-raspivid -t 0 -fl -ih -l -o - | tee test.h264 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:5000}' :demux=h264
+raspivid -t 0 -fl -ih -l -o - | tee $VIDEO_FILE | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:5000}' :demux=h264
 
 #end
