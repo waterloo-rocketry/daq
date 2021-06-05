fa770e883151d00ee822b8b5e04648a8ed97d2fe
Chris Sankey <5505109+chrissank@users.noreply.github.com>
2021-06-04 20:50:12 -0400

    Add simultaneous stream and save of footage

--- inflight_recording/autovid.sh
+++ inflight_recording/autovid.sh
@@ -4,6 +4,6 @@ NOW=$(date +%Y%m%d%H%M%S)
 
 VIDEO_FILE="/home/pi/autovid/video$NOW.h264"
 
-raspivid -t 0 -fl -o $VIDEO_FILE
+raspivid -t 0 -fl -ih -l -o - | tee $VIDEO_FILE | cvlc -vvv stream:///d^C/stdin --sout '#rtp{sdp=rtsp://:5000}' :demux=h264
 
 #end
